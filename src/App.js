c27b29f0edec3b4f2c2cfc37e23261c84b59c45d
Patrick <tunezepatrick@gmail.com>
2021-05-07 10:49:35 +0200

    done!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState();
+
+  const fetchUsers = async () => {
+    const response = await axios
+      .get("https://api.github.com/users")
+      .catch((e) => {
+        console.log(e);
+      });
+
+    setUsers(response);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          {users &&
+            users.data.map((user) => {
+              return (
+                <div key={user.id} className="col-md-3 col-lg-3 col-sm-1">
+                  <span>
+                    User: <b> {user.login}</b> with ID
+                    <strong>{user.id}</strong>
+                  </span>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
