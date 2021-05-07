eaf6bd9cddb2922676daf32cffc8785471baee9f
Patrick <tunezepatrick@gmail.com>
2021-05-07 10:35:21 +0200

    style

--- src/App.js
+++ src/App.js
@@ -21,21 +21,30 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          {users &&
-            users.data.map((user) => {
-              return (
-                <div key={user.id} className="col-md-3 col-lg-3 col-sm-1">
+    <div className="container-fluid app__container">
+      <div className="row app__container--users">
+        {!users ? (
+          <h6>Loading..., pls wait</h6>
+        ) : (
+          users.data.map((user) => {
+            return (
+              <div key={user.id} className="user col-md-2 col-lg-2 col-sm-1">
+                <div className="user-details">
                   <span>
-                    User: <b> {user.login}</b> with ID
+                    Type of the user is <strong>{user.type}</strong>
+                  </span>
+                  <span className="user-name">
+                    Name of the user is <strong>{user.login}</strong>
+                  </span>
+                  <span>
+                    <strong>{user.login}</strong>'s ID is{" "}
                     <strong>{user.id}</strong>
                   </span>
                 </div>
-              );
-            })}
-        </div>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
